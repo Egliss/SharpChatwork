b9d478de7f5405a1d89e9db24a40c2bdd6a07cc8
Egliss <egoparadise.rest@gmail.com>
2019-06-21 19:55:17 +0900

    Add Initial Commit

---
+++ Client/ChatworkError.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Client
+{
+    public class ChatworkQueryInfo
+    {
+        public int statusCode { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ Client/OAuth2Client.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpChatwork.Client
+{
+    public class OAuth2Client// : IChatworkClient
+    {
+        public IChatworkQueryResult Query(IChatworkQuery query)
+        {
+            throw new NotImplementedException();
+        }
+        /*
+        public async Task<IChatworkQueryResult> QueryAsync(IChatworkQuery query)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(query.endPoint);
+        }
+
+        public void Initialize()
+        {
+            AccessTokenQuery query = AccessTokenQuery.CreateFirst(authCode);
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = EndPoints.Token,
+            };
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Basic",
+                Convert.ToBase64String(Encoding.ASCII.GetBytes($"{ChatworkApplication.Client}:{ChatworkApplication.Secret}")));
+            request.Content = new FormUrlEncodedContent(URLArgEncoder.FromData(query));
+            HttpClient client = new HttpClient();
+
+            try
+            {
+                var result = await client.SendAsync(request);
+                using (StreamReader reader = new StreamReader(await result.Content.ReadAsStreamAsync()))
+                {
+                    return JsonConvert.DeserializeObject<AccessTokenQueryResult>(reader.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        */
+    }
+}
