ec8057939be431dc6876768a331c59906c3eba0e
Egliss <egoparadise.rest@gmail.com>
2019-06-24 18:13:31 +0900

    Fix/Add ArgEncoder invalid call

--- Client/URLArgEncoder.cs
+++ Client/URLArgEncoder.cs
@@ -8,22 +8,28 @@ namespace SharpChatwork.Client
 {
     class URLArgEncoder
     {
-        public static Dictionary<string, string> FromData<T>(T input)
+        public static Dictionary<string, string> ToDictionary<T>(T input)
         {
-            return typeof(T).GetMembers()
+            var type = typeof(T);
+            return type.GetMembers()
                 .Where(m => m.MemberType == MemberTypes.Field)
                 .Where(m => m.DeclaringType.IsPublic)
-                .Select(m => new KeyValuePair<string, object>(m.Name, typeof(T).GetField(m.Name).GetValue(input)))
+                .Select(m => new KeyValuePair<string, object>(m.Name, type.GetField(m.Name).GetValue(input)))
                 .Where(m => m.Value != null)
                 .Select(m => new KeyValuePair<string, string>(m.Key, m.Value.ToString()))
                 .Concat(
-                    typeof(T).GetMembers()
+                    type.GetProperties()
                     .Where(m => m.MemberType == MemberTypes.Property)
                     .Where(m => m.DeclaringType.IsPublic)
-                    .Select(m => new KeyValuePair<string, object>(m.Name, typeof(T).GetProperty(m.Name).GetValue(input)))
+                    .Where(m => type.GetProperty(m.Name).CanRead)
+                    .Select(m => new KeyValuePair<string, object>(m.Name, type.GetProperty(m.Name).GetValue(input)))
                     .Where(m => m.Value != null)
                     .Select(m => new KeyValuePair<string, string>(m.Key, m.Value.ToString())))
                 .ToDictionary(m => m.Key, n => n.Value);
+        }
+        public static string ToURLArg<T>(T input)
+        {
+            return "?" + string.Join(';' , ToDictionary(input).Select(m => $"{m.Key}={m.Value}"));
         }
     }
 }
