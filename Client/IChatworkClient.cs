f0ab0014995e3484ed78da9d7fcab306c49c7d51
Egliss <egoparadise.rest@gmail.com>
2019-06-24 21:02:07 +0900

    Add Contact and IncomingRequest

--- Client/IChatworkClient.cs
+++ Client/IChatworkClient.cs
@@ -7,9 +7,16 @@ namespace SharpChatwork
 {
     public interface IChatworkClient : ISerializable
     {
-        Task<List<Room>> GetRooms();
         Task<User> GetMeAsync();
-        Task<List<UserTask>> GetMyTasksAsync();
         Task<Status> GetMyStatusAsync();
+        Task<List<UserTask>> GetMyTasksAsync();
+
+        Task<List<Contact>> GetContactsAsync();
+
+        Task<List<Room>> GetRoomsAsync();
+
+        Task<List<IncomingRequest>> GetIncomingRequestsAsync();
+        Task<IncomingRequest> AcceptIncomingRequest(long requestId);
+        Task CancelIncomingRequestAsync(long requestId);
     }
 }
---
+++ Query/Types/IncomingRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Query.Types
+{
+    public class IncomingRequest
+    {
+        public int request_id { get; set; }
+        public int account_id { get; set; }
+        public string message { get; set; }
+        public string name { get; set; }
+        public string chatwork_id { get; set; }
+        public int organization_id { get; set; }
+        public string organization_name { get; set; }
+        public string department { get; set; }
+        public string avatar_image_url { get; set; }
+    }
+}
