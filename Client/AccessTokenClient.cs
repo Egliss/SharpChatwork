9016480e4e281413bcb17e22191a9fe3ebfba421
Egliss <egoparadise.rest@gmail.com>
2019-06-24 20:24:56 +0900

    Add GetRooms

---
+++ Client/AccessTokenClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using SharpChatwork.Client.Query.Rooms;
+using SharpChatwork.Query;
+
+namespace SharpChatwork
+{
+    public class AccessTokenClient : IChatworkClient
+    {
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected AccessTokenClient(SerializationInfo info, StreamingContext context)
+        {
+        }
+
+
+        Task<List<Room>> IChatworkClient.GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
