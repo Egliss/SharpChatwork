7596068d2569396a3a7b07a9481266e25273a7f9
Egliss <egoparadise.rest@gmail.com>
2019-06-24 20:43:41 +0900

    Clean move files

---
+++ Query/My/Task.cs
@@ -0,0 +1,20 @@
+﻿using SharpChatwork.Query.Me;
+using SharpChatwork.Query.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Query.My
+{
+    public class Task
+    {
+        public int task_id { get; set; }
+        public Room room { get; set; }
+        public User assigned_by_account { get; set; }
+        public string message_id { get; set; }
+        public string body { get; set; }
+        public int limit_time { get; set; }
+        public string status { get; set; }
+        public string limit_type { get; set; }
+    }
+}
