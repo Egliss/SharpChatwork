a44c5697cffb0b2685e82858572b268f645cc81b
Egliss <syunya.tokunaga@drecom.co.jp>
2019-06-24 21:29:55 +0900

    Add some interface and types

---
+++ Query/Types/MessageReadUnread.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Query.Types
+{
+    public class MessageReadUnread
+    {
+        public int unread_num { get; set; }
+        public int mention_num { get; set; }
+    }
+}
