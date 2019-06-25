8cdd1d5ad7376817af368076ee232d4549dde760
tokunaga_syunya <syunya.tokunaga@drecom.co.jp>
2019-06-26 08:33:47 +0900

    Add types

---
+++ Query/Types/InviteLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Query.Types
+{
+    public class InviteLink
+    {
+        public bool @public { get; set; }
+        public string url { get; set; }
+        public bool need_acceptance { get; set; }
+        public string description { get; set; }
+    }
+}
