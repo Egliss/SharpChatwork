48dbfc493cc22ad8fece674797f59697a3176a5b
Egliss <egoparadise.rest@gmail.com>
2019-06-25 09:24:47 +0900

    Add IdTypes

---
+++ Query/Types/Ids/TaskId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpChatwork.Query.Types.Ids
+{
+    public class TaskId : ElementId
+    {
+        public long task_id
+        {
+            get => this.id;
+            set => this.id = value;
+        }
+    }
+}
