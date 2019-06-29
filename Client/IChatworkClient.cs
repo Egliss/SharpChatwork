43698339c3faeb3d5a86d8bc44667104c3f03dc8
Egliss <egoparadise.rest@gmail.com>
2019-06-29 15:43:13 +0900

    Update Client implement

--- Client/IChatworkClient.cs
+++ Client/IChatworkClient.cs
@@ -1,7 +1,10 @@
 ﻿using SharpChatwork.Client;
 using SharpChatwork.Client.Query;
 using SharpChatwork.Query.Types;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
@@ -13,27 +16,36 @@ namespace SharpChatwork
 		IMeQuery me { get; }
 		IRoomQuery room { get; }
 		IContactQuery contact { get; }
-		IIncomingRequestQuery IncomingRequest { get; }
+		IIncomingRequestQuery incomingRequest { get; }
+	}
+	internal interface IInternalChatworkClient
+	{
+		ValueTask<string> QueryTextAsync(Uri uri, HttpMethod method, Dictionary<string, string> data);
+		ValueTask<ReturnT> QueryAsync<ReturnT>(Uri uri, HttpMethod method, Dictionary<string, string> data);
+		ValueTask QueryAsync(Uri uri, HttpMethod method, Dictionary<string, string> data);
 	}
 
-	public abstract class ChatworkClient : IChatworkClient
+	internal abstract class ChatworkClient : IChatworkClient, IInternalChatworkClient
 	{
 		public ChatworkClient()
 		{
 			this.me = new MeQuery(this);
 			this.contact= new ContactQuery(this);
 			this.room = new RoomQuery(this);
-			this.IncomingRequest = new IncomingRequestQuery(this);
+			this.incomingRequest = new IncomingRequestQuery(this);
 		}
 
-		internal protected abstract string clientName { get; }
-		internal protected abstract AuthenticationHeaderValue authenticationHeader { get; }
+		protected abstract string clientName { get; }
 
 		public IMeQuery me { get; private set; }
 		public IRoomQuery room { get; private set; }
 		public IContactQuery contact { get; private set; }
-		public IIncomingRequestQuery IncomingRequest { get; private set; }
+		public IIncomingRequestQuery incomingRequest { get; private set; }
 
 		public abstract void GetObjectData(SerializationInfo info, StreamingContext context);
+
+		public abstract ValueTask<string> QueryTextAsync(Uri uri, HttpMethod method, Dictionary<string, string> data);
+		public abstract ValueTask<ReturnT> QueryAsync<ReturnT>(Uri uri, HttpMethod method, Dictionary<string, string> data);
+		public abstract ValueTask QueryAsync(Uri uri, HttpMethod method, Dictionary<string, string> data);
 	}
 }
---
+++ Client/OAuth2/OAuth2Client2.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpChatwork.Client.OAuth2
+{
+	internal class OAuth2Client2 : ChatworkClient
+	{
+		protected override string clientName => nameof(OAuth2Client2);
+
+		private string clientKey { get; set; } = string.Empty;
+		private string secretKey { get; set; } = string.Empty;
+		private string oauth2Code { get; set; } = string.Empty;
+		private string accessToken { get; set; } = string.Empty;
+		private string refleshToken { get; set; } = string.Empty;
+		private long tokenExpired { get; set; } = 0;
+		private string scope { get; set; } = string.Empty;
+		private string redirectUri { get; set; } = string.Empty;
+		private DateTime tokenQueryTime { get; set; } = DateTime.Now;
+
+		public override void GetObjectData(SerializationInfo info, StreamingContext context)
+		{
+			throw new NotImplementedException();
+		}
+		public OAuth2Client2() { }
+		protected OAuth2Client2(SerializationInfo info, StreamingContext context)
+		{
+			this.clientKey = info.GetString(nameof(this.clientKey));
+			this.secretKey = info.GetString(nameof(this.secretKey));
+			this.oauth2Code = info.GetString(nameof(this.oauth2Code));
+			this.accessToken = info.GetString(nameof(this.accessToken));
+			this.refleshToken = info.GetString(nameof(this.refleshToken));
+			this.tokenExpired = info.GetInt64(nameof(this.tokenExpired));
+			this.tokenQueryTime = info.GetDateTime(nameof(this.tokenQueryTime));
+			this.redirectUri = info.GetString(nameof(this.redirectUri));
+			this.scope = info.GetString(nameof(this.scope));
+		}
+
+		public override async ValueTask<ReturnT> QueryAsync<ReturnT>(Uri uri, HttpMethod method, Dictionary<string, string> data)
+		{
+			var text = await this.QueryTextAsync(uri, method, data);
+			return JsonConvert.DeserializeObject<ReturnT>(text);
+		}
+		public override async ValueTask QueryAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
+		{
+			HttpRequestMessage request = new HttpRequestMessage
+			{
+				Method = method,
+				RequestUri = uri
+			};
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.accessToken);
+			request.Content = new FormUrlEncodedContent(data);
+			HttpClient client = new HttpClient();
+			await client.SendAsync(request);
+			return;
+			//using (StreamReader reader = new StreamReader(await result.Content.ReadAsStreamAsync(), Encoding.UTF8))
+			//{
+			//	return reader.ReadToEnd();
+			//}
+		}
+		public override async ValueTask<string> QueryTextAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
+		{
+			HttpRequestMessage request = new HttpRequestMessage
+			{
+				Method = method,
+				RequestUri = uri
+			};
+			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this.accessToken);
+			request.Content = new FormUrlEncodedContent(data);
+			HttpClient client = new HttpClient();
+			var result = await client.SendAsync(request);
+			using (StreamReader reader = new StreamReader(await result.Content.ReadAsStreamAsync(), Encoding.UTF8))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
