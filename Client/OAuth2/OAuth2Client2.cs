cb4262fd2a1b4c322adce77cca21ac3c8fa944e3
tokunaga_syunya <syunya.tokunaga@drecom.co.jp>
2019-07-01 06:24:35 +0900

    Update restructure client

--- Client/OAuth2/OAuth2Client2.cs
+++ Client/OAuth2/OAuth2Client2.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
+using SharpChatwork.Query;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -78,6 +80,69 @@ namespace SharpChatwork.Client.OAuth2
 			using (StreamReader reader = new StreamReader(await result.Content.ReadAsStreamAsync(), Encoding.UTF8))
 			{
 				return reader.ReadToEnd();
+			}
+		}
+
+		public OAuth2ConcentQueryResult Authorization(OAuth2ConcentQuery query, string codeVerifer = "")
+		{
+			query.client_id = this.clientKey;
+			this.scope = query.scope;
+			this.redirectUri = query.redirect_uri;
+
+			var concentUrlArg = EndPoints.Oauth2.OriginalString + $"{URLArgEncoder.ToURLArg(query)}";
+			Console.WriteLine("Please input code of redirect url code=");
+			Process.Start(new ProcessStartInfo("cmd", $"/c start {concentUrlArg}") { CreateNoWindow = true });
+			this.oauth2Code = Console.ReadLine();
+
+			if (string.IsNullOrEmpty(this.oauth2Code))
+			{
+				return new OAuth2ConcentQueryResult()
+				{
+					error = "oauth_code_error"
+					//error_description = "inputed oauth_code is null or empty",
+				};
+			}
+			query.client_id = this.clientKey;
+			return new OAuth2ConcentQueryResult()
+			{
+				code = this.oauth2Code,
+			};
+		}
+		public async Task<OAuth2TokenQueryResult> UpdateToken(OAuth2TokenQuery.GrantType grantType = OAuth2TokenQuery.GrantType.RefreshToken, string codeVerifer = "")
+		{
+			OAuth2TokenQuery tokenQuery = new OAuth2TokenQuery(grantType)
+			{
+				scope = this.scope,
+				redirect_uri = this.redirectUri,
+			};
+			HttpRequestMessage request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Post,
+				RequestUri = EndPoints.Token,
+			};
+
+			if (grantType == OAuth2TokenQuery.GrantType.AuthroizationCode)
+				tokenQuery.code = this.oauth2Code;
+			else if (grantType == OAuth2TokenQuery.GrantType.RefreshToken)
+				tokenQuery.refresh_token = this.refleshToken;
+
+			request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+				"Basic",
+				Convert.ToBase64String(Encoding.ASCII.GetBytes($"{this.clientKey}:{this.secretKey}")));
+			request.Content = new FormUrlEncodedContent(URLArgEncoder.ToDictionary(tokenQuery));
+
+			HttpClient client = new HttpClient();
+			var response = await client.SendAsync(request);
+			var stream = await response.Content.ReadAsStreamAsync();
+
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				var result = JsonConvert.DeserializeObject<OAuth2TokenQueryResult>(reader.ReadToEnd());
+				this.tokenExpired = result.expires_in;
+				this.tokenQueryTime = DateTime.Now;
+				this.refleshToken = result.refresh_token;
+				this.accessToken = result.access_token;
+				return result;
 			}
 		}
 	}
