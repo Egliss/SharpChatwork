539db7e7d07a7e425f5e941f08c79ae8b181295b
Egliss <egoparadise.rest@gmail.com>
2019-07-11 17:57:24 +0900

    Clean ChatworkQueryError

--- Client/ChatworkQueryError.cs
+++ Client/ChatworkQueryError.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SharpChatwork
 {
-    internal class ChatworkQueryInfo
+    internal class ChatworkQueryError
     {
         public int statusCode { get; set; }
         public string message { get; set; }
