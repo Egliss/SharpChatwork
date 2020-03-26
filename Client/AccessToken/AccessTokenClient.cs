a101ff82dbc126a86a3bd9627d0aa9387923fed3
Egliss <egoparadise.rest@gmail.com>
2020-03-26 16:29:07 +0900

    Add AccessTokenClientを実装

--- Client/AccessToken/AccessTokenClient.cs
+++ Client/AccessToken/AccessTokenClient.cs
@@ -1,35 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.Serialization;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using SharpChatwork.Query.Types;
 
-namespace SharpChatwork
+namespace SharpChatwork.AccessToken
 {
-	internal class AccessTokenClient : ChatworkClient
-	{
-		internal override string clientName => nameof(AccessTokenClient);
+    [Serializable]
+    public class AccessTokenClient : ChatworkClient
+    {
+        internal override string clientName => nameof(AccessTokenClient);
+
+        private string accessToken { get; set; } = string.Empty;
 
         public override void GetObjectData(SerializationInfo info, StreamingContext context)
         {
-            throw new NotImplementedException();
+            info.AddValue(nameof(this.accessToken), this.accessToken);
+        }
+        public AccessTokenClient(string accessToken)
+        {
+            this.accessToken = accessToken;
+        }
+        protected AccessTokenClient(SerializationInfo info, StreamingContext context)
+        {
+            this.accessToken = info.GetString(nameof(this.accessToken));
         }
 
-        internal override ValueTask<ReturnT> QueryAsync<ReturnT>(Uri uri, HttpMethod method, Dictionary<string, string> data)
+        internal override async ValueTask<ReturnT> QueryAsync<ReturnT>(Uri uri, HttpMethod method, Dictionary<string, string> data)
         {
-            throw new NotImplementedException();
+            var text = await this.QueryTextAsync(uri, method, data);
+            return JsonConvert.DeserializeObject<ReturnT>(text);
         }
 
-        internal override ValueTask QueryAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
+        internal override async ValueTask QueryAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
         {
-            throw new NotImplementedException();
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = uri
+            };
+            request.Headers.Authorization = new AuthenticationHeaderValue("X-ChatWorkToken", this.accessToken);
+            if (data.Count != 0)
+                request.Content = new FormUrlEncodedContent(data);
+            HttpClient client = new HttpClient();
+            await client.SendAsync(request);
+            return;
         }
 
-        internal override ValueTask<string> QueryTextAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
+        internal override async ValueTask<string> QueryTextAsync(Uri uri, HttpMethod method, Dictionary<string, string> data)
         {
-            throw new NotImplementedException();
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = method,
+                RequestUri = uri
+            };
+            request.Headers.Add("X-ChatWorkToken", this.accessToken);
+            if (data.Count != 0)
+                request.Content = new FormUrlEncodedContent(data);
+            HttpClient client = new HttpClient();
+            var result = await client.SendAsync(request);
+            using (StreamReader reader = new StreamReader(await result.Content.ReadAsStreamAsync(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
