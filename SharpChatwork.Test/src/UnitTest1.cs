0419b95b6f534a96327dfa9982d4a614fab69b34
syunya_tolkunaga <syunya.tokunaga@drecom.co.jp>
2023-08-04 18:38:15 +0900

    Add default

---
+++ SharpChatwork.Test/src/UnitTest1.cs
@@ -0,0 +1,14 @@
+using SharpChatwork.AccessToken;
+
+namespace SharpChatwork.Test.src
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var client = new AccessTokenClient('');
+
+        }
+    }
+}
