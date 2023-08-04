f9c31d299799cdfe97d2c0aa37cbda7c28701b53
syunya_tolkunaga <syunya.tokunaga@drecom.co.jp>
2023-08-04 17:17:31 +0900

    Add lint by sdk

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,10 +10,5 @@ if [ ! "${USE_FIXLINT}" ]; then
   FORMAT_FIX_OPTIONS=" --verify-no-changes "
 fi
 
-dotnet tool install dotnet-format \
-  --add-source https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet6/nuget/v3/index.json
-
-dotnet tool run dotnet-format -- whitespace ${FORMAT_SHARED_OPTIONS} ${FORMAT_FIX_OPTIONS}
-dotnet tool run dotnet-format -- style ${FORMAT_SHARED_OPTIONS} ${FORMAT_FIX_OPTIONS}
-
-dotnet tool uninstall dotnet-format
+dotnet dotnet format whitespace ${FORMAT_SHARED_OPTIONS} ${FORMAT_FIX_OPTIONS}
+dotnet dotnet format style ${FORMAT_SHARED_OPTIONS} ${FORMAT_FIX_OPTIONS}
