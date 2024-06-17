a7b74085eb995ded279ab0f460adb280e4d47a3b
syunya_tolkunaga <syunya.tokunaga@drecom.co.jp>
2024-06-17 09:51:48 +0900

    Fix

--- SharpChatwork/src/Client/QueryResult/Ids/UserId.cs
+++ SharpChatwork/src/Client/QueryResult/Ids/UserId.cs
@@ -4,7 +4,7 @@ namespace SharpChatwork.Query.Types
 {
     public class UserId : ElementId
     {
-        public long user_id
+        public string user_id
         {
             get => this.id;
             set => this.id = value;
