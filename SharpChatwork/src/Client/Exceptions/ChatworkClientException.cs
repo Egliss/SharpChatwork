d157988089ee6d12db855e8f2706ed3df783aa0c
syunya_tolkunaga <syunya.tokunaga@drecom.co.jp>
2024-11-26 19:08:44 +0900

    Add exception message

--- SharpChatwork/src/Client/Exceptions/ChatworkClientException.cs
+++ SharpChatwork/src/Client/Exceptions/ChatworkClientException.cs
@@ -5,4 +5,9 @@ namespace SharpChatwork.Client.Exceptions;
 public class ChatworkClientException(ResponseWrapper response) : Exception
 {
     public readonly ResponseWrapper Response = response;
+
+    public override string ToString()
+    {
+        return base.ToString() + $" --> ChatworkClientException: {this.Response.statusCode} {this.Response.content}";
+    }
 }
